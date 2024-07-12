953b6f5417dd62eabfedc045569620ff3036ed87
matheusmu22 <matheusmusantos91@gmail.com>
2024-07-12 12:36:29 -0300

    Update dashboard_hospitalar_streamlit.py

--- dashboard_hospitalar_streamlit.py
+++ dashboard_hospitalar_streamlit.py
@@ -2,11 +2,11 @@ import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
 
-# Dados fictícios para os meses de janeiro a junho de 2024
+# Dados para os meses de janeiro a junho de 2024
 data = {
     'Month': ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'],
     'Occupancy Rate': [80, 85, 75, 90, 95, 88],
-    'Infection Rate': [2, 1.8, 2.5, 2.2, 2, 1.5],
+    'Infection Rate': [0.21, 0.53, 0.47, 0.10, 0.41, 0],
     'Mortality Rate': [1, 1.2, 1.1, 1.5, 1.3, 1.2],
     'Satisfaction Rate': [90, 92, 85, 88, 91, 89],
     'Vaginal Births': [150, 160, 145, 170, 165, 155],
